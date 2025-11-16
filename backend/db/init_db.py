baaccab8a0beb93a958615766d1e6bae41a5405c
Suryansh-Sharan <suryansh123.sharan@gmail.com>
2025-11-16 17:14:02 +0530

    Fixed init_db.py

--- backend/db/init_db.py
+++ backend/db/init_db.py
@@ -1,18 +1,20 @@
 import os
 import psycopg2
 from dotenv import load_dotenv
+
 load_dotenv()
 
 try:
     conn = psycopg2.connect(
-            host="localhost",
-            database="sapienone",
-            user=os.environ['DB_USERNAME'],
-            password=os.environ['DB_PASSWORD'])
+        host=os.getenv("DB_HOST", "localhost"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USERNAME"),
+        password=os.getenv("DB_PASSWORD")
+    )
 
     cur = conn.cursor()
 
-    cur.execute('DROP TABLE IF EXISTS contact;')
+    cur.execute('DROP TABLE IF EXISTS contacts;')
 
     create_table_sql = """
     CREATE TABLE IF NOT EXISTS contacts (
@@ -24,12 +26,12 @@ try:
     );
     """
 
-    
     cur.execute(create_table_sql)
     print("Table created successfully")
     conn.commit()
 
     cur.close()
     conn.close()
-except Exception as error:
-    print(error)
+
+except Exception as e:
+    print(f"An error occurred: {e}")
